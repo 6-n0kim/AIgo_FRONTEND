23649ddc12ded96bf0df787bdb5eb9945ab34368
김준영 <1090jun@gmail.com>
2026-01-04 16:45:38 +0900

    V1.1.1 린트에러 수정

--- src/pages/Study/ProblemTestPage.tsx
+++ src/pages/Study/ProblemTestPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ProblemTestPage = () => {
   return <div>ProblemTestPage</div>;
 };
--- src/pages/Study/StudyPage.tsx
+++ src/pages/Study/StudyPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const StudyPage = () => {
   return <div>StudyPage</div>;
 };
